99e34e17dd7cb1a3e0c4518c48e26181ac53f756
anks2807 <ankitsinghal2807@gmail.com>
2026-01-14 22:53:42 +0530

    prompt management

--- src/main/java/com/ttn/springai/controller/ChatController.java
+++ src/main/java/com/ttn/springai/controller/ChatController.java
@@ -2,24 +2,31 @@ package com.ttn.springai.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@AllArgsConstructor
 @Slf4j
 public class ChatController {
 
     private final ChatClient chatClient;
     @Qualifier("chatClientWithMemory")
     private final ChatClient chatClientWithMemory;
+    @Value("classpath:prompts/system-prompt.st")
+    Resource systemTemplate;
+
+    public ChatController(ChatClient chatClient, @Qualifier("chatClientWithMemory") ChatClient chatClientWithMemory) {
+        this.chatClient = chatClient;
+        this.chatClientWithMemory = chatClientWithMemory;
+    }
+
+
+     
 
     @GetMapping("/chat")
     public String chat(@RequestParam String message) {
@@ -31,6 +38,15 @@ public class ChatController {
     public String chat(@RequestParam String message, @RequestHeader String chatId) {
         return chatClientWithMemory.prompt()
         .advisors(spec -> spec.param("chatId", chatId))
+        .user(message).call().content();
+    }
+
+
+    @GetMapping("/chatWithHRBot")
+    public String chatWithHRBot(@RequestParam String message, @RequestHeader String chatId) {
+        return chatClientWithMemory.prompt()
+        .advisors(spec -> spec.param("chatId", chatId))
+        .system(systemTemplate)
         .user(message).call().content();
     }
 
